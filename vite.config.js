83f594055ae2a4a9670d4625900ff5bdbfaea864
InnocentBoy-007 <mechanshil7@gmail.com>
2025-01-15 01:10:54 +0530

    Made some changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/Restaurant-Admin/', // Replace with your GitHub repo name
-});
+    server: {
+        port: 4000,
+        host: true
+    }
+})
