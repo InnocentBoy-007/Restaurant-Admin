730ffbafa764c34fb3d56e1d0806a3d30b847043
unknown <mechanshil7@gmail.com>
2025-01-19 23:57:37 +0530

    Fixed some minor bugs

--- src/pages/OtpVerify.jsx
+++ src/pages/OtpVerify.jsx
@@ -57,25 +57,36 @@ export const OtpVerify = () => {
       Cookies.remove("adminToken");
       Cookies.remove("adminRefreshToken");
       navigate("/admin/signup");
-      alert(`${error.response.data.message}. Please signup again!`);
+      if (error.response) {
+        alert(`${error.response.data.message}. Please signup again!`);
+      } else if (error.request) {
+        alert("Network error! Please try again later!");
+      } else {
+        console.log(
+          "An unexpected error occured while trying to confirm the OTP!"
+        );
+      }
     }
   };
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <div className="bg-white p-6 rounded shadow-md w-96">
-        <input
-          type="text"
-          placeholder="Enter your OTP here..."
-          value={OTP}
-          onChange={(e) => setOTP(e.target.value)}
-          className="w-full p-2 border border-gray-300 rounded mb-4"
-        />
-        <button
-          className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
-          onClick={confirmOTP}
-        >
-          {laoding ? "confirming..." : "confirm OTP"}
-        </button>
+        <form onSubmit={confirmOTP}>
+          <input
+            type="text"
+            placeholder="Enter your OTP here..."
+            value={OTP}
+            onChange={(e) => setOTP(e.target.value)}
+            className="w-full p-2 border border-gray-300 rounded mb-4"
+          />
+          <button
+            className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
+            type="submit"
+            onClick={confirmOTP}
+          >
+            {laoding ? "confirming..." : "confirm OTP"}
+          </button>
+        </form>
       </div>
     </div>
   );
