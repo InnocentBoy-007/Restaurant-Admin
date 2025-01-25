0862d99233c7c7332abfd5aad5a810f7c64131e8
InnocentBoy-007 <mechanshil7@gmail.com>
2025-01-26 01:10:41 +0530

    Made a minor change in the repository location

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: 'Restaurant-Admin',
+    base: '/Restaurant-Admin/',
     server: {
         port: 4000,
         host: true
